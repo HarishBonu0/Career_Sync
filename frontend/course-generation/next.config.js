b0031201b3b531ffc29e36fe708cad10397b72ed
Harish-Bonu03 <harish.bonu03@gmail.com>
2026-01-30 06:11:53 +0530

    Fix Next alias resolution for @ paths

--- frontend/course-generation/next.config.js
+++ frontend/course-generation/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,6 +8,10 @@ const nextConfig = {
   experimental: {
     // Allow importing modules from ../db shared folder
     externalDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 }
 
